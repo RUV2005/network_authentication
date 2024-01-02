06c55548c2e243bd224122961a617e330775d60b
文龙 程 <2099246846@qq.com>
2024-01-02 10:00:49 +0800

    deleted:    main.cmd

--- main.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=C:\Users\ThinkPad\AppData\Local\Programs\Python\Python38;%PATH%
-set PYTHONHOME=C:\Users\ThinkPad\AppData\Local\Programs\Python\Python38
-"%~dp0main.exe" %*
